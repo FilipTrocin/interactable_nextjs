08e0d10ccd6d3edb6a81e590e0b94a9747eb4b45
FilipTrocin <mxtrocinski@gmail.com>
2023-07-06 16:52:00 +0200

    applied css to `Card`

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,13 +1,21 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import Image from "next/image";
+
+// CSS
+import cardStyles from '@/components/card.module.css'
+
+// Fonts
+import {Koulen} from "next/font/google";
+const koulen = Koulen({ weight: '400', subsets: ['latin']})
+
 
 export default function Card(props) {
     return (
-        <a className="bg" href={props.link} >
+        <a className={cardStyles.bg} href={props.link} >
             <a className={props.hoverCard}>
-                <div className="content">
-                    <p>{props.title}</p>
-                    <div className="inside-bg">
+                <div className={cardStyles.content}>
+                    <p className={koulen.className}>{props.title}</p>
+                    <div className={cardStyles.insideBg}>
                         <Image src={props.img} width={281} height={315} alt={props.alt}/>
                     </div>
                 </div>
