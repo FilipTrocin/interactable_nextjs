4bbc96c9249200219cf168e4e493300ddcc1ad3d
FilipTrocin <mxtrocinski@gmail.com>
2023-08-04 09:00:10 +0100

    shifted `icons` dir under public to serve static files

--- components/info_panel/decision_trees.js
+++ components/info_panel/decision_trees.js
@@ -1,25 +1,25 @@
 export const testimonials =
     [
         {
-         icon: "../info_panel/icons/creditcard-img.svg",
+         icon: "/icons/creditcard-img.svg",
          title: "Credit Risk Assessment",
          desc: "The decision tree model analyse the customer's credit score, income, employment history, and other " +
              "factors to determine whether to approve or reject a loan application."
         },
         {
-         icon: "../info_panel/icons/doctor-img.svg",
+         icon: "/icons/doctor-img.svg",
          title: "Healthcare Diagnosis",
          desc: "The doctors can confirm or exclude various medical conditions based on Decision Trees. The nodes " +
              "conditions can be for instance: symptoms, patient’s age, underlying medical problems etc."
         },
         {
-         icon: "../info_panel/icons/quality-img.svg",
+         icon: "/icons/quality-img.svg",
          title: "Manufacture quality controls",
          desc: "Upon completion of product production, the final product proceeds to  undergo through  " +
              "decision tree-based quality control checks to ensure the produced good meets company’s standards."
         },
         {
-         icon: "../info_panel/icons/van-img.svg",
+         icon: "/icons/van-img.svg",
          title: "Supply chain optimisation",
          desc: "Transportation industry eagerly takes for decision trees to optimise the routes for their vans to reduce" +
              " their costs. Rather than relying on the guesswork, decision trees can help to make informed decision " +
