79762f2423070f539788752ec3ee52aff8a82b8c
FilipTrocin <mxtrocinski@gmail.com>
2023-08-06 07:47:45 +0100

    adjusted articles styling

--- app/pages/article/decision_trees/page.js
+++ app/pages/article/decision_trees/page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {testimonials} from "@/components/info_panel/decision_trees.js";
+import styles from "@/app/pages/article/article.module.css";
 
 // TODO: transfer common things to template and abstract the code
 
@@ -23,34 +24,36 @@ export default function DecTrees() {
     const technique = "decision_trees"
 
     return (
-        <div className="App">
-            <div id="infoPanel">
-                <div className="headerDiv">
-                    <h1>Did you know <span>{technique.toUpperCase().replaceAll("_", " ")}</span> is commonly used in:
-                    </h1>
-                </div>
-                <div id="testimonials-box">
-                    {testimonials.map((testimonial, i) => <Testimonial key={i} testimonial={testimonial}/>)}
-                </div>
+        <div className={styles.app}>
+            <a href="/">
+                <div className={styles.banner}></div>
+            </a>
+            <div className={styles.infoPanel}>
+               <div className={styles.headerDiv}>
+                  <h1>Did you know <span>{technique.toUpperCase().replaceAll("_", " ")}</span> is commonly used in:</h1>
+               </div>
+               <div className={styles.testimonialsBox}>
+                  {testimonials.map((testimonial, i) => <Testimonial key={i} testimonial={testimonial} />)}
+               </div>
             </div>
-            <section className='scrolling-section'>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <a href="#main" style={{color: "#2272FF"}}>
-                    Scroll
-                    {Array(3).fill().map((_, i) => <span key={i}/>)}
-                </a>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
+            <section className={styles.scrollingSection}>
+            <a href="#mainNotebook" style={{color: "#2272FF"}}>
+              Scroll to understand the way it works!
+              {Array(3).fill().map((_, i) => <span key={i} />)}
+            </a>
             </section>
-            <div id="main">
+            <div className={styles.notebook} id="mainNotebook">
                 <iframe width="100%" height="1000" frameBorder="0"
-                        src="https://observablehq.com/embed/3b4097c305719cef?cell=*">
+                    src="https://observablehq.com/embed/3b4097c305719cef?cell=*">
                 </iframe>
+            </div>
+            <div>
+                <div>
+                  <p><a href="/" className={styles.footerTitle}>Interactable</a></p>
+                    <p>Privacy Policy</p>
+                    <p>Terms & Conditions</p>
+                  <p>Filip Trociński, 2023 © All rights reserved</p>
+                </div>
             </div>
         </div>
     )
--- app/pages/article/k_nearest_neighbours/page.js
+++ app/pages/article/k_nearest_neighbours/page.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {testimonials} from "@/components/info_panel/k_nearest_neighbours";
 
+// CSS
+import styles from "@/app/pages/article/article.module.css";
+
 // TODO: transfer common things to template and abstract the code
 
 function Testimonial({testimonial}) {
   return (
-    <div className='testimonial'>
-      <div className='testimonial-icon'>
+    <div className={styles.testimonial}>
+      <div>
         <img src={testimonial.icon} alt={testimonial.icon.match(/\/([^\/]+)\.\w+$/)[1]} />
       </div>
-      <div className='testimonial-title'>
+      <div className={styles.testimonialTitle}>
         <h3>{testimonial.title}</h3>
       </div>
-      <div className='testimonial-txt'>
+      <div>
         <p>{testimonial.desc}</p>
       </div>
     </div>
@@ -23,33 +26,36 @@ export default function Knn() {
     const technique = "k_nearest_neighbours"
 
     return (
-        <div className="App">
-            <div id="infoPanel">
-              <div className="headerDiv">
+        <div className={styles.app}>
+            <a href="/">
+                <div className={styles.banner}></div>
+            </a>
+            <div className={styles.infoPanel}>
+               <div className={styles.headerDiv}>
                   <h1>Did you know <span>{technique.toUpperCase().replaceAll("_", " ")}</span> is commonly used in:</h1>
-              </div>
-              <div id="testimonials-box">
+               </div>
+               <div className={styles.testimonialsBox}>
                   {testimonials.map((testimonial, i) => <Testimonial key={i} testimonial={testimonial} />)}
-              </div>
+               </div>
             </div>
-            <section className='scrolling-section'>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <a href="#main" style={{color: "#2272FF"}}>
-              Scroll
+            <section className={styles.scrollingSection}>
+            <a href="#mainNotebook" style={{color: "#2272FF"}}>
+              Scroll to understand the way it works!
               {Array(3).fill().map((_, i) => <span key={i} />)}
             </a>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
             </section>
-            <div id="main">
+            <div className={styles.notebook} id="mainNotebook">
                 <iframe width="100%" height="1000" frameBorder="0"
                     src="https://observablehq.com/embed/c2c4aa78b69f979f@427?cell=*">
                 </iframe>
+            </div>
+            <div>
+                <div>
+                  <p><a href="/" className={styles.footerTitle}>Interactable</a></p>
+                    <p>Privacy Policy</p>
+                    <p>Terms & Conditions</p>
+                  <p>Filip Trociński, 2023 © All rights reserved</p>
+                </div>
             </div>
         </div>
     );
